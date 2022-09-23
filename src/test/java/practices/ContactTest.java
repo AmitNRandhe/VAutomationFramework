d501b524f5c9a0f4ce93573c670f0f9cb8969c77
AmitNRandhe <amitrandheanr@gmail.com>
2022-09-23 13:11:10 +0530

    First commit - Engg 1

---
+++ src/test/java/practices/ContactTest.java
@@ -0,0 +1,13 @@
+package practices;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void contact()
+	{
+		System.out.println("First commit - MavenProject");
+	}
+
+}
